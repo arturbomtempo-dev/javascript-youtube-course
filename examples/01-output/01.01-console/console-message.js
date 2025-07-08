a6310d36080d217f9c1eaafbaa984109003b1e37
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-07-08 15:39:46 -0300

    docs: add comments to console message examples for clarity

--- examples/01-output/01.01-console/console-message.js
+++ examples/01-output/01.01-console/console-message.js
@@ -1,1 +1,2 @@
+// Exibe uma mensagem no console do navegador
 console.log("Olá! Seja bem-vindo ao curso de JavaScript.");
--- examples/01-output/01.03-display-variable-value/display-variable-value.js
+++ examples/01-output/01.03-display-variable-value/display-variable-value.js
@@ -1,2 +1,5 @@
+// Declara uma variável chamada name e atribui o valor "Artur"
 let name = "Artur";
+
+// Exibe no console o texto junto com o valor da variável name
 console.log("O nome armazenado é: " + name);
