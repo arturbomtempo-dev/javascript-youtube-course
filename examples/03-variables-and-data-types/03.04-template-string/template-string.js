1fc31fc4781169298465d0af4081413353f21282
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-07-09 12:27:45 -0300

    feat: add template string example for variable interpolation

---
+++ examples/03-variables-and-data-types/03.04-template-string/template-string.js
@@ -0,0 +1,8 @@
+const nome = "Artur";
+const idade = 19;
+
+// Forma antiga
+console.log("Olá, meu nome é " + nome + " e tenho " + idade + " anos.");
+
+// Forma moderna
+console.log(`Olá, meu nome é ${nome} e tenho ${idade} anos.`);  
